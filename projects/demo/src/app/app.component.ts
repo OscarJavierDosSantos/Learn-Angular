3e737e562f6e4b44f147f738c4a236adb86a5130
Oscar Javier <oscarjavierdos@gmail.com>
2025-03-12 09:18:00 +0100

    Add Header, Footer and Menu components

--- projects/demo/src/app/app.component.ts
+++ projects/demo/src/app/app.component.ts
@@ -1,18 +1,23 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { Patata } from './patata';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { MenuComponent } from './components/menu/menu.component';
 
 @Component({
   selector: 'cas-root',
-  imports: [RouterOutlet, Patata],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent, MenuComponent],
   template: `
-    <h1>Welcome to {{ title }}!</h1>
-    <cas-patata></cas-patata>
-
-    <router-outlet />
+    <cas-header>
+      <cas-menu />
+    </cas-header>
+    <main>
+      <p>1</p>
+      <p>2</p>
+      <router-outlet />
+    </main>
+    <cas-footer />
   `,
   styles: [],
 })
-export class AppComponent {
-  title = 'demo';
-}
+export class AppComponent {}
